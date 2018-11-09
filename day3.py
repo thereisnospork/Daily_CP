d1b786a3acb7dda6a0c57d89e813ad46ae1c7cd2
George <georLeonard@gmail.com>
2018-11-08 23:39:44 -0800

    need to better define binary tree before breadth first search then reverse tree filling (breadth 1st)

--- day3.py
+++ day3.py
@@ -3,31 +3,13 @@
 # which deserializes the string back into the tree.
 #
 
-import numpy as np
 
-def serialize_root(de_serialized):
 
-    tree = dict()
-    for i, each in enumerate(de_serialized):
-        if i % 3 == 0: #identifying each root
-            children = []
-            try:
-                children.append(de_serialized[i+1])
-            except:
-                None
-            try:
-                children.append(de_serialized[i+2])
-            except:
-                None
-            tree[each]=children
+
 #### first layer done / working, need to add recursion
     ###also redefine as Node/root / L / R sub children and etc.
     print(tree)
-
+    return(tree)
 def de_serialize_root():
     None
 
-de_serialized = np.arange(1,51)
-print(de_serialized)
-
-serialize_root(de_serialized)
